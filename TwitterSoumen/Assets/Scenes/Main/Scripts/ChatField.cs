748cba38ccd3c32aa01df06f479e3ae4071143a1
Masatomo Segawa <masa-segawa@masa-segawa.com>
2016-05-12 23:05:15 +0900

    チャットフィールドの追加

---
+++ TwitterSoumen/Assets/Scenes/Main/Scripts/ChatField.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChatField : MonoBehaviour {
+
+	// 自分自身のキャンバスグループ.
+	private CanvasGroup canvasGroup;
+
+	// アクティブかどうか.
+	private bool isActive;
+
+	// Use this for initialization
+	void Start () {
+	
+		canvasGroup = this.GetComponent<CanvasGroup> ();
+
+		// アクティブにしない.
+		isActive = false;
+
+	}
+
+	/// <summary>
+	/// アクティブ/非アクティブを変える
+	/// </summary>
+	public void SwitchActive(){
+		isActive = !isActive;
+
+		if (isActive) {
+			SetActive ();
+		} else {
+			SetUnActive ();
+		}
+
+	}
+
+	/// <summary>
+	/// アクティブにする.
+	/// </summary>
+	private void SetActive(){
+
+		this.canvasGroup.interactable = true;
+		this.canvasGroup.alpha = 1.0f;
+		this.canvasGroup.blocksRaycasts = true;
+
+	}
+
+	/// <summary>
+	/// 非アクティブにする.
+	/// </summary>
+	private void SetUnActive(){
+
+		this.canvasGroup.interactable = false;
+		this.canvasGroup.alpha = 0.0f;
+		this.canvasGroup.blocksRaycasts = false;
+
+	}
+
+
+}
---
+++ TwitterSoumen/Assets/Scenes/Main/Scripts/UserControll.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public delegate void OnUserGetKeyDown();
+
+public class UserControll : MonoBehaviour {
+
+	public  UnityEngine.Events.UnityEvent enterKeyInput;
+
+	// Update is called once per frame
+	void Update () {
+	
+		KeyInput ();
+
+
+	}
+		
+	/// <summary>
+	/// チャットフィールドを選択する.
+	/// </summary>
+	private void SelectChatField(){
+
+
+
+	}
+
+	/// <summary>
+	/// キーのインプット入力を受け取る.
+	/// </summary>
+	private void KeyInput(){
+	
+		// Enterキーが押されたら.
+		if (Input.GetKeyDown (KeyCode.Return)) {
+			enterKeyInput.Invoke();
+		}
+
+
+	}
+		
+}
